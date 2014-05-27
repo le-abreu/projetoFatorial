73e35f9c7ae2f01cbcf165444200bf16f1622519
Leandro Abreu Ferreira <le_abreu@live.com>
2014-05-27 01:31:44 -0300

    Classes Teste Fatorail criado para testar o claculo do Fatorial

---
+++ TesteFatorial.java
@@ -0,0 +1,9 @@
+public class TesteFatorial 
+{
+	public static void main(String[] args)
+	{
+	 	Fatorial fatorial = new Fatorial();
+		System.out.println(fatorial. calcularFatorial(fatorial.calcularFatorial(5)));
+	}
+	
+}
