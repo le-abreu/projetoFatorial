84b48d4606ad331c9010f10bc125a605cd2062f1
Leandro Abreu Ferreira <le_abreu@live.com>
2014-06-26 09:17:49 -0300

    Calculo do fatorial de 4

--- testes/TesteFatorial.java
+++ testes/TesteFatorial.java
@@ -4,8 +4,8 @@ public class TesteFatorial
 	public static void main(String[] args)
 	{
 	 	Fatorial fatorial = new Fatorial();
-		int numero = fatorial.calcularFatorial(fatorial.calcularFatorial(5));
-		System.out.println(numero);
+		int r = fatorial.calcularFatorial(fatorial.calcularFatorial(4));
+		System.out.println(r);
 	}
 	
 }
